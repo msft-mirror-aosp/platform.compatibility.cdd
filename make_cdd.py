f3c78821531b7def0fbbb77dfe04d59f3e4bae46
Gina Dimino <gdimino@google.com>
2019-01-23 10:46:16 -0800

    CDD build script and helper shell script.
    Test: ./cdd_gen.sh --version <version-number> --branch <branch-name>
    Change-Id: Iaec01dd19385623b7d53462969d8f604ffde45c9

--- make_cdd.py
+++ make_cdd.py
@@ -23,8 +23,6 @@ import tidylib
 
 
 HEADERS_FOR_TOC = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7']
-global ANDROID_VERSION
-ANDROID_VERSION = "7.0, (N)"
 TOC_PER_COL = 34
 
 def get_section_info(my_path):
@@ -61,10 +59,10 @@ def get_section_info(my_path):
   return section_info
 
 
-def get_soup(section_info):
+def get_soup(section_info, version):
   html_body_text = u'''<!DOCTYPE html>
 <head>
-<title>Android ''' + ANDROID_VERSION + ''' Compatibility Definition</title>
+<title>Android ''' + version + ''' Compatibility Definition</title>
 <link rel="stylesheet" type="text/css" href="source/android-cdd.css"/>
 <meta charset="utf-8" />
 </head>
@@ -194,7 +192,7 @@ def main():
                }
 
   # Generate the HTML for PDF
-  soup = get_soup(section_info)
+  soup = get_soup(section_info, ANDROID_VERSION)
   add_id_to_section_headers(soup)
   add_toc(soup)
   html = soup.prettify(formatter='html')
